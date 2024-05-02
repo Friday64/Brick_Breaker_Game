974b5cf43a41fe89142e106c3c276e9d1d0d360d
Matthew Frantz <mcfrantz12@gmail.com>
2024-05-02 11:37:29 -0400

    ball_pos
    Changed starting point of ball

--- Brick_Breaker_Main_Code.py
+++ Brick_Breaker_Main_Code.py
@@ -10,7 +10,7 @@ width, height = 1920, 1080
 screen = pygame.display.set_mode((width, height))
 
 # Ball settings
-ball_pos = [width // 2, height // 2]
+ball_pos = [width // 960, height // 540]
 ball_radius = 15
 ball_color = (255, 0, 0)
 velocity = [2, 2]  # X and Y velocity
