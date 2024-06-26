9a4e51c71c3bc1a1427e5047a8ac25c243f1a9a1
Matthew Frantz <mcfrantz12@gmail.com>
2024-06-26 12:38:03 -0400

    Created Function to Draw Score
    increments the score by 10 when a brick is removed

--- Brick_Breaker_Main_Code.py
+++ Brick_Breaker_Main_Code.py
@@ -76,6 +76,12 @@ def draw_tries(tries):
     text = font.render("Tries: " + str(tries), True, WHITE)
     screen.blit(text, (width - 300, 20))
 
+# Function to draw the scoreon left-hand side of the screen
+def draw_score():
+    font = pygame.font.SysFont(None, 50)
+    text = font.render("Score: " + str(score), True, WHITE)
+    screen.blit(text, (20, 20))
+
 # Function to draw bricks
 def draw_bricks():
     for row in bricks:
@@ -154,7 +160,8 @@ def ball_behavior():
         for brick in row:
             #brick collision with ball(when brick_dianameter touches ball)
             if (brick["rect"].colliderect(ball_pos[0] - ball_radius, ball_pos[1] - ball_radius, 2 * ball_radius, 2 * ball_radius)):
-                row.remove(brick)
+                row.remove(brick) 
+                score += 10
                 velocity[1] = -velocity[1]
                 break
 
@@ -189,6 +196,7 @@ def start_game():
 
         screen.fill(BLACK)
         draw_tries(tries)
+        draw_score(score)
         draw_paddle(paddle_pos_x, paddle_pos_y)
         move_paddle()
         draw_bricks()
